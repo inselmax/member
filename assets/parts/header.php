370d07a968a00c2e774aeb864fd3c2cb9f2bcd09
yanagi-insel <yanagi.inselart@gmail.com>
2021-03-10 18:01:32 +0900

    リンク調整

--- assets/parts/header.php
+++ assets/parts/header.php
@@ -2,8 +2,8 @@
 <div class="Header">
   <div class="header-inner">
     <div class="header-box">
-      <h1 class="site-logo"><a href="/member/"><img src="/member/assets/img/logo.svg" alt="若杉ビル"></a></h1>
-      <p class="site-name">仲介事業者様専用サイト</p>
+      <h1 class="site-logo"><a href="/"><img src="/member/assets/img/logo.svg" alt="若杉ビル"></a></h1>
+      <p class="site-name"><a href="/member/">仲介事業者様専用サイト</a></p>
     </div>
   </div>
 </div>
