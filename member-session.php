b97d2a28196590e13a0219def9d3641ef0182e17
yanagi-insel <yanagi.inselart@gmail.com>
2021-03-17 14:43:33 +0900

    セッション・各ファイル読み込み

--- member-session.php
+++ member-session.php
@@ -1,3 +1,8 @@
 <?php
+@session_start();
 
+$Root = $_SERVER['DOCUMENT_ROOT'];
+require_once( $Root . '/member/config.php' );
+require_once( $Root . '/member/func.php');
+require_once( $Root . '/member/htmllib.php' );
 ?>
